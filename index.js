c93effe6ad3d76a7aedad0cb6266ebd928091ba7
Rayhan Islam <merayhanislam21@gmail.com>
2026-01-31 20:23:24 +0600

    redux

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 const INCREMENT = 'INCREMENT'
+import { createStore } from 'redux';
 const DECREMENT = 'DECREMENT'
 
 const initialCounterState = {
     count: 0,
+    username: 'Guest',
 }
 
-const incrementCounter = ()=>{
+const incrementCounter = () => {
     return {
-        type : INCREMENT,
+        type: INCREMENT,
     }
 }
 
-const decrementCOunter = ()=>{
+const decrementCOunter = () => {
     return {
-        type : DECREMENT,
+        type: DECREMENT,
     }
 }
+
+const counterReducer = (state = initialCounterState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1,
+            }
+
+
+
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1,
+            }
+
+
+        default:
+            return state;
+    }
+}
+//create a store
+const store = createStore(counterReducer);
+
+store.subscribe(()=>{
+    console.log('State updated:', store.getState());
+})
+
+//dispatching actions
+store.dispatch(incrementCounter());
